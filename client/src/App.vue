4436f7b93f98fadb6d3c0a46e6eeebf72daa9a0c
Daniils Titovs <mrmiirflinn@gmail.com>
2025-07-25 09:12:28 +0300

    Init commit + admin dashboard layout

---
+++ client/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <ThemeProvider>
+    <Suspense>
+      <SidebarProvider>
+        <RouterView />
+      </SidebarProvider>
+    </Suspense>
+  </ThemeProvider>
+</template>
+
+<script setup lang="ts">
+import ThemeProvider from './components/layout/ThemeProvider.vue'
+import SidebarProvider from './components/layout/SidebarProvider.vue'
+</script>
---
+++ client/src/main.js
@@ -0,0 +1,28 @@
+import './assets/main.css'
+// Import Swiper styles
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import 'jsvectormap/dist/jsvectormap.css'
+import 'flatpickr/dist/flatpickr.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from '@/router/index.js'
+import VueApexCharts from 'vue3-apexcharts'
+import axios from 'axios'
+import {createPinia} from 'pinia'
+
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+axios.defaults.withCredentials = true;
+axios.defaults.withXSRFToken = true;
+axios.defaults.baseURL = "127.0.0.1:8000";
+
+const app = createApp(App)
+const store = createPinia()
+
+app.use(store)
+app.use(router)
+app.use(VueApexCharts)
+
+app.mount('#app')
